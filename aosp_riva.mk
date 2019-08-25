137da21b8489392a14c1339aa004d70caa5c453b
RaySlash <stevemathewjoy@gmail.com>
2019-08-25 19:28:14 +0530

    riva: aex-ify and update dependencies

--- aosp_riva.mk
+++ aosp_riva.mk
@@ -21,16 +21,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Inherit from riva device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+# Boot Animation
+TARGET_BOOT_ANIMATION_RES := 720
+
+EXTENDED_BUILD_TYPE := OFFICIAL
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := riva
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 5A
-PRODUCT_NAME := lineage_riva
+PRODUCT_NAME := aosp_riva
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
